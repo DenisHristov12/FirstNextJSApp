07914e0629f2a27e433f72a8b22225cc7f9ceff4
Denis Hristov <48285397+DenisHristov12@users.noreply.github.com>
2024-11-17 18:53:43 +0200

    add new component

---
+++ components/main-header/main-header.js
@@ -0,0 +1,39 @@
+import classes from './main-header.module.css';
+
+import Link from 'next/link';
+import Image from 'next/link';
+import logoImg from '@/assets/logo.png';
+import MainHeaderBackground from './main-header-background';
+
+function MainHeader() {
+  return (
+    <>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link
+          className={classes.logo}
+          href="/">
+          <Image
+            src={logoImg}
+            alt="A plate with food on it"
+            priority
+          />
+          NextLevel Food
+        </Link>
+
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <Link href="/meals">Browse Meals</Link>
+            </li>
+            <li>
+              <Link href="/community">Foodies Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
+
+export default MainHeader;
